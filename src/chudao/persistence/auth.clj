bf5e49866179ab8b7ca169030054b9b52e46c4a6
Zach Chen <zacharymorn@gmail.com>
2016-07-07 20:14:02 -0400

    Encrypt password in db

--- src/chudao/persistence/auth.clj
+++ src/chudao/persistence/auth.clj
@@ -1,23 +1,25 @@
 (ns chudao.persistence.auth
   (:import [java.sql SQLException])
-  (:require [korma.core :as korma]))
+  (:require [korma.core :as korma]
+            [crypto.password.scrypt :as password]))
 
 (korma/defentity User)
 
 (defn login
   [username password]
-  (->
-    (korma/select User
-                  (korma/fields :UserId :UserName)
-                  (korma/where {:UserName username :Password password}))
-    first))
+  (let [user-data (first (korma/select User
+                                       (korma/fields :UserId :UserName :Password)
+                                       (korma/where {:UserName username})))
+        encrypted-password (:Password user-data)]
+    (if (password/check password encrypted-password)
+      user-data)))
 
 (defn register
   [username password]
   (try
     (->
       (korma/insert User
-                  (korma/values {:UserName username :Password password}))
+                  (korma/values {:UserName username :Password (password/encrypt password)}))
       :generated_key)
     (catch SQLException e
       (case (.getErrorCode e)
